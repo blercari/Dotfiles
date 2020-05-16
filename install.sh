e5062897c667f0685dc8515636fe1fd042322c7e
Bruno Lercari <brunolercari@gmail.com>
2020-05-16 20:20:39 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dotfiles_dir=$(dirname $(readlink -f $0))
+
+mkdir -p $HOME/.config
+
+# Neovim
+rm -rf $HOME/.config/nvim
+ln -s $dotfiles_dir/.config/nvim $HOME/.config
+
+# Qtile
+rm -rf $HOME/.config/qtile
+ln -s $dotfiles_dir/.config/qtile $HOME/.config
+
+# Awesome
+rm -rf $HOME/.config/awesome
+ln -s $dotfiles_dir/.config/awesome $HOME/.config
